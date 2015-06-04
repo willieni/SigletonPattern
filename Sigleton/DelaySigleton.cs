0ee9c70948dc46ad6d08b80808a36f534f040eac
willieni <niliwei2@163.com>
2015-06-04 12:23:36 +0800

    添加 延迟加载单例类

---
+++ Sigleton/DelaySigleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SigletonPattern.Sigleton
+{
+    public class DelaySigleton
+    {
+        private DelaySigleton()
+        {
+        }
+
+        public static DelaySigleton Instance
+        {
+            get
+            {
+                return Nested.instance;
+            }
+        }
+
+        class Nested
+        {
+            static Nested()
+            {
+            }
+
+            internal static readonly DelaySigleton instance = new DelaySigleton();
+        }
+    }
+}
